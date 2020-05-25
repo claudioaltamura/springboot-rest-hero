ece5115bcc07617fe8e316321ee41259ca317883
Claudio Altamura <claudio@claudioaltamura.de>
2020-05-25 20:35:43 +0200

    new: added controller

---
+++ src/main/java/de/claudioaltamura/springboot/rest/controller/HeroController.java
@@ -0,0 +1,49 @@
+package de.claudioaltamura.springboot.rest.controller;
+
+import de.claudioaltamura.springboot.rest.model.HeroRequest;
+import de.claudioaltamura.springboot.rest.model.HeroRequestWithId;
+import de.claudioaltamura.springboot.rest.model.HeroResponse;
+import de.claudioaltamura.springboot.rest.service.HeroService;
+import java.net.URI;
+import java.util.Collection;
+import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class HeroController {
+
+  private final HeroService heroService;
+
+  @GetMapping("/api/v1/heroes")
+  public ResponseEntity<Collection<HeroResponse>> findAll() {
+    return ResponseEntity.ok(heroService.findAll());
+  }
+
+  @PostMapping("/api/v1/heroes")
+  public ResponseEntity<HeroResponse> add(@RequestBody @Valid HeroRequest heroRequest) {
+    //Optional?
+    HeroResponse hero = heroService.add(heroRequest);
+
+    if (hero == null)
+      return ResponseEntity.noContent().build();
+
+    URI location = ServletUriComponentsBuilder.fromCurrentRequest().path(
+        "/{id}").buildAndExpand(hero.getId()).toUri();
+
+    return ResponseEntity.created(location).body(hero);
+  }
+
+}
