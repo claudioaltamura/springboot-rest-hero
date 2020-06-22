c2eb6ae8eb93f32a7c986f8e9aa1f9af5992be1f
Claudio Altamura <claudio@claudioaltamura.de>
2020-06-22 18:03:23 +0200

    new: added configuration test

---
+++ src/test/java/de/claudioaltamura/springboot/rest/HeroApplicationTests.java
@@ -0,0 +1,21 @@
+package de.claudioaltamura.springboot.rest;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import de.claudioaltamura.springboot.rest.controller.HeroController;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroApplicationTests {
+
+	@Autowired
+	private HeroController controller;
+
+	@Test
+	void contextLoads() {
+		assertThat(controller).isNotNull();
+	}
+
+}
